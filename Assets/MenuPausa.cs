f83ff7f2ca62634addf2de03adfcd9f9be4c9b70
BenjaminCruz-A01747811 <95510233+BenjaminCruz-A01747811@users.noreply.github.com>
2023-04-17 16:56:02 -0600

    Escena Pausa

---
+++ Assets/MenuPausa.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPausa : MonoBehaviour
+{
+    [SerializeField] private GameObject botonPausa;
+
+    [SerializeField] private GameObject menuPausa;
+
+    private bool juegoPausado = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (juegoPausado)
+            {
+                Reanudar();
+            }
+            else
+            {
+                Pausa();
+            }
+        }   
+    }
+    public void Pausa()
+    {
+        juegoPausado = true;
+        Time.timeScale = 0f;
+        botonPausa.SetActive(false);
+        menuPausa.SetActive(true);
+    }
+
+    public void Reanudar()
+    {
+        juegoPausado = false;
+        Time.timeScale = 1f;
+        botonPausa.SetActive(true);
+        menuPausa.SetActive(false);
+    }
+
+    public void Reiniciar()
+    {
+        juegoPausado = false;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Cerrar()
+    {
+        Debug.Log("Cerrando juego"); 
+        Application.Quit();
+    }
+}
